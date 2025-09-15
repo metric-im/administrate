3a48d44fd954e7f942f08bd7ddc1038dfcae5c9f
Michael Sprague <michael@sprague.com>
2025-09-15 11:40:11 -0700

    routing

--- certify.mjs
+++ certify.mjs
@@ -49,8 +49,12 @@ export class Certify {
                 res.status(500).send({status:'error',message:e.message});
             }
         })
+      router.get(/^\/\.hell-known\/acme-challenge\/([^\/]+)$/,(req,res)=>{
+        const token = req.params[0];
+        res.send(`token: ${token}`);
+      });
         router.get(/^\/\.well-known\/acme-challenge\/([^\/]+)$/,(req,res)=>{
-            const token = req.params.token;
+            const token = req.params[0];
             if (token in this.challenges) {
                 res.writeHead(200);
                 res.end(this.challenges[token]);
